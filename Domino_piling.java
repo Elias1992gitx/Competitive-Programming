30ce2bb9940b413fba6b70cc0d510331579770a1
Elias1992gitx <107762465+Elias1992gitx@users.noreply.github.com>
2022-09-18 12:14:51 +0300

    Create Domino_piling.java

---
+++ Domino_piling.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ 
+public class Domino_piling {
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        int m=input.nextInt();
+        int n=input.nextInt();
+            int result=(m*n)/2;
+            System.out.println(result);
+        }
+    }
